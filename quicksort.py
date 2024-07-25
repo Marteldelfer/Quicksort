bec130fa7d15958091a3e0cb379695a01c3ee653
Murillo Ferraz <marteldelfer@gmail.com>
2024-07-24 23:52:36 -0300

    Update__name__.py
    Creates if block, allowing it to be imported without getting errors or unwanted code running

--- quicksort.py
+++ quicksort.py
@@ -1,13 +1,6 @@
 #Autoral
 #Muito mais eficiênte que ordenação por seleção, porém tem um limite de recursão
 
-
-from random import randint as rand
-from time import time
-lista = [rand(1,10) for i in range(990)]
-
-start = time()
-
 def quicksort(lista: list) -> list:
     listaesq = []
     listadir = []
@@ -21,5 +14,11 @@ def quicksort(lista: list) -> list:
             listadir.append(i)
     return quicksort(listaesq) + [num] + quicksort(listadir)
 
-print(quicksort(lista))
-print(time() - start)
+if __name__ == "__main__":
+    from random import randint as rand
+    from time import time
+    
+    lista = [rand(1,10) for i in range(990)]
+    start = time()
+    print(quicksort(lista))
+    print(time() - start)
